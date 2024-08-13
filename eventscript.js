44088a8025d93d5f54b9c36230d6105db6c7af58
Nithin <nithinkoushik05@gmail.com>
2024-08-13 23:53:47 +0530

    Currency conveter project building Day-1

--- eventscript.js
+++ eventscript.js
@@ -1,9 +1,9 @@
-let btn = document.querySelector("#box");
-console.dir(btn);
-btn.onmouseover = () => {
-    console.log("You have hovered on me");
+// let btn = document.querySelector("#box");
+// console.dir(btn);
+// btn.onmouseover = () => {
+//     console.log("You have hovered on me");
 
-}
+// }
 
 let clk = document.querySelector("button");
 clk.ondblclick = () => {
